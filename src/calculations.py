285380cc97792685f118b15f107f45ba31f4e761
Christian Haller <75713652+Delotrium@users.noreply.github.com>
2022-10-19 21:47:55 +1000

    added nm, yrd, m, ft conversions

--- src/calculations.py
+++ src/calculations.py
@@ -1,7 +1,6 @@
 import discord
 from discord.ext import commands
 
-numlist = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."]
 
 class Calculator(commands.Cog):
     def __init__(self, client):
@@ -33,6 +32,30 @@ class Calculator(commands.Cog):
             nm = round(amount / 6076.11549,3)
             yrd = round(amount /3,3)
             await ctx.send(f"Conversions for {ctx.author.mention}:\n{query} can be converted to the following (3dp):\nKilometres = {km}km\nMetres = {metres}m\nMiles = {mi}mi\nNautical Miles = {nm}nm\nYards = {yrd}yrd")
+        elif query.lower().__contains__("m") and not query.lower().__contains__("nm"):
+            amount = float(query[:-1])
+            km = round(amount /1000, 3)
+            ft = round(amount*3.2808399, 3) 
+            mi = round(amount /1609.344, 3)
+            nm = round(amount / 1852,3)
+            yrd = round(amount * 1.0936133,3)
+            await ctx.send(f"Conversions for {ctx.author.mention}:\n{query} can be converted to the following (3dp):\nKilometres = {km}km\nFeet = {ft}ft\nMiles = {mi}mi\nNautical Miles = {nm}nm\nYards = {yrd}yrd")
+        elif query.lower().__contains__("nm"):
+            amount = float(query[:-2])
+            km = round(amount *1.852, 3)
+            ft = round(amount*6076.11549, 3) 
+            mi = round(amount *1.15077945, 3)
+            m = round(amount * 1852, 3)
+            yrd = round(amount * 2025.3718,3)
+            await ctx.send(f"Conversions for {ctx.author.mention}:\n{query} can be converted to the following (3dp):\nKilometres = {km}km\nFeet = {ft}ft\nMiles = {mi}mi\nMetres = {m}m\nYards = {yrd}yrd")
+        elif query.lower().__contains__("yrd"):
+            amount = float(query[:-3])
+            km = round(amount /1093.6133, 3)
+            ft = round(amount*3, 3) 
+            mi = round(amount / 1760, 3)
+            m = round(amount * 0.9144, 3)
+            nm = round(amount / 2025.37183,3)
+            await ctx.send(f"Conversions for {ctx.author.mention}:\n{query} can be converted to the following (3dp):\nKilometres = {km}km\nFeet = {ft}ft\nMiles = {mi}mi\nMetres = {m}m\nNautical Miles = {nm}nm")
 
 def setup(client):
     client.add_cog(Calculator(client))
