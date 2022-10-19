a8b0f360c72e6c71240a306a27016387baf6b582
Christian Haller <75713652+Delotrium@users.noreply.github.com>
2022-10-19 20:11:07 +1000

    Created quick-links commands

---
+++ src/links.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+
+class Link(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command(aliases=['link.youtube'])
+    async def link_youtube(self, ctx):
+        await ctx.send(f'{ctx.author.mention}: https://www.youtube.com')
+    
+    @commands.command()
+    async def link(self, ctx, *, query):
+        await ctx.send(f'{ctx.author.mention}: https://{query}.com')
+
+def setup(client):
+    client.add_cog(Link(client))
