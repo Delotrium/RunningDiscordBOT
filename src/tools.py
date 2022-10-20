247fbaccdd7ae6273acb1dddf9d7b4e080014fc4
Christian Haller <delotrium@outlook.com>
2022-10-20 11:13:53 +1000

    change client to bot (does not affect code)

--- src/tools.py
+++ src/tools.py
@@ -2,8 +2,8 @@ import discord
 from discord.ext import commands
 
 class Tools(commands.Cog):
-    def __init__(self, client):
-        self.client = client
+    def __init__(self, bot):
+        self.bot = bot
 
     @commands.command(aliases=["pm"])
     async def dm(self, ctx, member : discord.Member, *, message):
@@ -11,5 +11,5 @@ class Tools(commands.Cog):
         Author = ctx.author
         await Author.send("\""+ message + f"\" was sent to {member}!")
         
-async def setup(client):
-    await client.add_cog(Tools(client))
+async def setup(bot):
+    await bot.add_cog(Tools(bot))
