0a1ad9c9fc423388c6b7a7b19b16cac02c8ee323
Christian Haller <delotrium@outlook.com>
2022-10-20 11:12:30 +1000

    created logging and cleaned code

--- bot.py
+++ bot.py
@@ -2,29 +2,35 @@ import discord
 from discord.ext import commands
 import os
 import asyncio
+import logging
+
 
 intents = discord.Intents().all() 
-client = commands.Bot(command_prefix='r!', intents=intents)
+intents.message_content = True
+bot = commands.Bot(command_prefix='r!', intents=intents)
 
 
-@client.command()
+@bot.command()
 async def load(ctx, extension):
-    await client.load_extension(f"src.{extension}")
+    await bot.load_extension(f"src.{extension}")
 
-@client.command()
+@bot.command()
 async def unload(ctx, extension):
-    await client.unload_extension(f"src.{extension}")
+    await bot.unload_extension(f"src.{extension}")
 
 async def load_extensions():
     for filename in os.listdir("./src"):
         if filename.endswith(".py"):
             # cut off the .py from the file name
-            await client.load_extension(f"src.{filename[:-3]}")
+            await bot.load_extension(f"src.{filename[:-3]}")
 
 
 async def main():
-    async with client:
+    
+    async with bot:
         await load_extensions()
-        await client.start('')
+        await bot.start('')
+discord.utils.setup_logging()
+
 
 asyncio.run(main())
