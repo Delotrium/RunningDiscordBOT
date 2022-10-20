3652a748c15a2e195e6e96b818cc2135d6e8b7bf
Christian Haller <delotrium@outlook.com>
2022-10-20 15:40:53 +1000

    added wr command

--- src/dicordembed.py
+++ src/dicordembed.py
@@ -71,5 +71,37 @@ class DiscordEmbeds(commands.Cog):
             em.set_image(url = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F2.bp.blogspot.com%2F-ei0gkJakS9Q%2FWrgu-eV-kUI%2FAAAAAAAAFZY%2FfrLECpnaqhg9ZoCf7X7f5uuKESbtKG2IwCLcBGAs%2Fs1600%2Fmuscles%252B-%252Bfront%252Bview%252Bv2.jpg&f=1&nofb=1&ipt=ff6f38323d7d002c36e3d0fe9d4aa3a7ef6ab5966b9036b1ba1dc60213507b59&ipo=images")
             await ctx.send(embed=em)
 
+
+    @commands.command(aliases=["worldrecord", "worldrecords", "wrs"])
+    async def wr(self, ctx):
+        embed = discord.Embed()
+        colour = discord.Color.from_rgb(255, 0, 205)
+        embed.set_author(name="RunBOT Records")
+        embed.title = "Major World Records"
+        embed.colour = colour
+        embed.description = "The following list contains all the major world records associated with running."
+        embed.add_field(name='100m - Men', value="``9.58 seconds`` - ``Usain Bolt``", inline=False)
+        embed.add_field(name='100m - Women', value="``10.49 seconds`` - ``Florence Griffith-Joyner``", inline=False)
+        embed.add_field(name='200m - Men', value="``19.19seconds`` - ``Usain Bolt``", inline=False)
+        embed.add_field(name='200m - Women', value="``21.34 seconds`` - ``Florence Griffith-Joyner``", inline=False)
+        embed.add_field(name='400m - Men', value="``43.03seconds`` - ``Wayde van Niekerk``", inline=False)
+        embed.add_field(name='400m - Women', value="``47.60 seconds`` - ``Marita Koch``", inline=False)
+        embed.add_field(name='800m - Men', value="``1:40.91`` - ``David Rudisha``", inline=False)
+        embed.add_field(name='800m - Women', value="``1:53.28`` - ``Jarmila KratochvÃ­lovÃ¡``", inline=False)
+        embed.add_field(name='1 mile - Men', value="``3:43.13`` - ``Hicham El Guerrouj``", inline=False)
+        embed.add_field(name='1 mile - Women', value="``4:12.33`` - ``Sifan Hassan``", inline=False)
+        embed.add_field(name='5k - Men', value="``12:35.36`` - ``Joshua Cheptegei``", inline=False)
+        embed.add_field(name='5k - Women', value="``14:06.62`` - ``Letesenbet Gidey``", inline=False)
+        embed.add_field(name='10k - Men', value="``26:11.00`` - ``Joshua Cheptegei``", inline=False)
+        embed.add_field(name='10k - Women', value="``29:01.30`` - ``Letesenbet Gidey``", inline=False)
+        embed.add_field(name='HM - Men', value="``57:31`` - ``Jacob Kiplimo``", inline=False)
+        embed.add_field(name='HM - Women', value="``1:02:52`` - ``Letesenbet Gidey``", inline=False)
+        embed.add_field(name='Marathon - Men', value="``2:01:09`` - ``Eliud Kipchoge``", inline=False)
+        embed.add_field(name='Marathon - Women', value="``2:14:04`` - ``Brigid Kosgei``", inline=False)
+        embed.add_field(name='100k - Men', value="``6:05:41`` - ``Aleksandr Sorokin``", inline=False)
+        embed.add_field(name='100k - Women', value="``6:33:11`` - ``Tomoe Abe``", inline=False)
+        await ctx.send(ctx.author.mention)
+        await ctx.send(embed=embed)
+
 async def setup(client):
     await client.add_cog(DiscordEmbeds(client))
